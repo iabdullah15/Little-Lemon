9d5ecb2e23d610efbf2100700e70d50229e604bf
Abdullah Zubair <abdullahzubair2001@gmail.com>
2023-02-15 21:09:45 +0500

    Installed DRF and added it to the installed apps section in settings.py
    Added a serializers.py file to create a BookingSerializer for the Booking
    model and MenuSerializer for the Menu Model.
    
    Added a BookingViewSet inheriting the ModelViewSet class to display all
    bookings and add new bookings.
    
    Routed the BookingViewSet using the DefaultRouter.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,14 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('tables', views.BookingViewSet)
+
 
 urlpatterns = [
     path('', views.index, name = 'home'),
+    path('items/', views.MenuItemsView.as_view()),
+    path('item/<int:pk>/', views.SingleMenuItemView.as_view()),
+    path('', include(router.urls)),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 
@@ -7,3 +14,19 @@ def index(request):
 
     # return HttpResponse('Hello')
     return render(request, 'index.html')
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.UpdateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
