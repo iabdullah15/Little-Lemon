b16bd16a69d067eddfcf801e1cce5b46387e0d21
Abdullah Zubair <abdullahzubair2001@gmail.com>
2023-02-16 19:33:21 +0500

    Added tests to test the api endpoints in the tests folder.

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -17,5 +17,5 @@ class Menu(models.Model):
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField()
 
-    def __str__(self) -> str:
-        return self.Title
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
